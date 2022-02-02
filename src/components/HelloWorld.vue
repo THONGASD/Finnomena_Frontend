4b3c8588c063b0f2183f47583211f1f201e64208
thongzaa11 <phaka.th@inet.co.th>
2022-02-02 23:48:47 +0700

    Init project and call restAPI

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,45 +1,72 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <v-card>
+      <v-card-title>
+        Nutrition
+        <v-spacer></v-spacer>
+        <v-select
+          :items="fundRangeItems"
+          label="Solo field"
+          solo
+          v-model="fundRange"
+          @change="getData()"
+          hide-details
+        ></v-select>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-details
+        ></v-text-field>
+      </v-card-title>
+      <v-data-table
+        :headers="headers"
+        :items="fundsData"
+        :search="search"
+      ></v-data-table>
+    </v-card>
   </div>
 </template>
 
 <script>
+/* eslint-disable */
+const axios = require("axios");
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+
+  mounted() {
+    this.getData();
+  },
+  data() {
+    return {
+      fundsData: [],
+      search: "",
+      fundRangeItems: ["1D", "1W", "1M", "1Y"],
+      fundRange: "1D",
+      headers: [
+        {
+          text: "ชื่อกองทุน",
+
+          value: "thailand_fund_code",
+        },
+
+        { text: "ผลตอบแทน", value: "nav_return" },
+        { text: "ราคา", value: "nav" },
+        { text: "อัพเดตเมื่อ", value: "nav_date" },
+      ],
+    };
+  },
+  methods: {
+    async getData() {
+      const { data } = await axios(
+        `http://localhost:3000/${this.fundRange}`
+      );
+
+      this.fundsData = data.data;
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
